5828a4947f651b5949a21d98f452430dd54f8144
Mohammad Rubel Hossain <rubrex96@hotmail.com>
2020-04-11 20:27:34 +0600

    Landing page Landed

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className="navParent">
+          <div className="logo">RubeReX</div>
+          <div className="navigation">
+            <ul>
+              <li>
+                <button>Cats</button>
+              </li>
+              <li>
+                <button>Dogs</button>
+              </li>
+              <li>
+                <button>Rabits</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+      <main></main>
+      <footer>
+        <div className="footerContainer">
+          <div className="footerChild">
+            <h1>Flor De Muorto</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea
+              nesciunt aspernatur reprehenderit quod illo officia, fugiat odio
+              totam saepe ex voluptates repudiandae asperiores quos ab
+              repellendus illum, accusantium, quaerat tenetur.
+            </p>
+            <strong>
+              Subscribe to the site{" "}
+              <span role="img" aria-label="bell">
+                🔔
+              </span>
+            </strong>
+          </div>
+          <div className="footerChild">
+            <h1>Flor De Muorto</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea
+              nesciunt aspernatur reprehenderit quod illo officia, fugiat odio
+              totam saepe ex voluptates repudiandae asperiores quos ab
+              repellendus illum, accusantium, quaerat tenetur.
+            </p>
+            <strong>
+              Make a Donation{" "}
+              <span role="img" aria-label="money">
+                💰
+              </span>
+            </strong>
+          </div>
+          <div className="footerChild">
+            <h1>Flor De Muorto</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea
+              nesciunt aspernatur reprehenderit quod illo officia, fugiat odio
+              totam saepe ex voluptates repudiandae asperiores quos ab
+              repellendus illum, accusantium, quaerat tenetur.
+            </p>
+            <strong>
+              Contribute{" "}
+              <span role="img" aria-label="heart">
+                ❤️
+              </span>
+            </strong>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
