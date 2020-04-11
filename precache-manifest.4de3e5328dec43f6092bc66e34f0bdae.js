691d38039b097bffdbfaaff4db6b859235f07a85
Mohammad Rubel Hossain <rubrex96@hotmail.com>
2020-04-11 21:17:04 +0600

    Updates

---
+++ precache-manifest.4de3e5328dec43f6092bc66e34f0bdae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0e4503e9c5d4b6295179a6ba421f02",
+    "url": "/landingpage/index.html"
+  },
+  {
+    "revision": "8560d4453b3a7b6bdb73",
+    "url": "/landingpage/static/css/main.5e52cc88.chunk.css"
+  },
+  {
+    "revision": "0e452da62f4c73127627",
+    "url": "/landingpage/static/js/2.590268e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landingpage/static/js/2.590268e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8560d4453b3a7b6bdb73",
+    "url": "/landingpage/static/js/main.acc90c70.chunk.js"
+  },
+  {
+    "revision": "c9d097fb04f3d571f24c",
+    "url": "/landingpage/static/js/runtime-main.0a049e67.js"
+  },
+  {
+    "revision": "46833d4d59086ec078d9109774406e69",
+    "url": "/landingpage/static/media/nature.46833d4d.jpg"
+  }
+]);
