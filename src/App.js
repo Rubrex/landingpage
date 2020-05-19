4eaddf9b8624584ed08d2296c4142b4efd870fb8
Rubrex <rubrex96@hotmail.com>
2020-05-19 17:20:42 +0600

    Logo Changed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header>
         <div className="navParent">
-          <div className="logo">Textt Brunch</div>
+          <div className="logo">RubReX</div>
           <div className="navigation">
             <ul>
               <li>
